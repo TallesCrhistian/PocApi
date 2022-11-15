eb339ac9302b1c0896b0a356858c3e425556f454
Talles <Talles@DESKTOP-FJ1A91L>
2022-11-14 22:45:26 -0300

    Feat: Adicionando Tela

---
+++ PocAPI.WPF/FrmPrincipal.xaml.cs
@@ -0,0 +1,35 @@
+﻿using PocAPI.WPF.Cadastros;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PocAPI.WPF
+{
+    /// <summary>
+    /// Lógica interna para FrmPrincipal.xaml
+    /// </summary>
+    public partial class FrmPrincipal : Window
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void MeiCliente_Click(object sender, RoutedEventArgs e)
+        {
+            FrmLogin frmLogin = new FrmLogin();
+            frmLogin.Owner = this;
+            frmLogin.ShowDialog();
+        }
+    }
+}
--- PocApi.Compartilhado/ModeloDeVisualizacao/DocumetoAReceber/DocumentoAReceberInserirViewModel.cs
+++ PocApi.Compartilhado/ModeloDeVisualizacao/DocumetoAReceber/DocumentoAReceberInserirViewModel.cs
@@ -1,12 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PocApi.Compartilhado.ModeloDeVisualizacao.DocumetoAReceber
 {
-    class DocumentoAReceberIserirViewModel
+    public class DocumentoAReceberInserirViewModel
     {
+        public int IdCliente { get; set; }
+        public int IdPedido { get; set; }
+        public int IdPagamento { get; set; }
+        public int QuantidadeParcela { get; set; }
+        public DateTime DataLancamento { get; set; }
+        public DateTime DataVencimento { get; set; }
+        public DateTime DataJuros { get; set; }
+        public int Carencia { get; set; }
+        public decimal Valor { get; set; }
+        public decimal Restante { get; set; }
+        public decimal PercentualJuros { get; set; }
+        public decimal ValorPago { get; set; }
+        public ClienteViewModel ClienteDTO { get; set; }
+        public PedidoInserirViewModel PedidoDTO { get; set; }
+        public PagamentoInserirViewModel PagamentoDTO { get; set; }
     }
 }
